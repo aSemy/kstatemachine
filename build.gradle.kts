81593d25a24eb644824ef8e9faed8fb29924c579
Adam <897017+aSemy@users.noreply.github.com>
2022-01-08 10:23:24 +0100

    setup qoomon git versioning plugin (easier version management, based on git tag)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,19 @@
+
+plugins {
+    id("me.qoomon.git-versioning") version "5.1.2"
+}
+
 group = "ru.nsk"
-version = "0.9.0"
+version = "0.0.0-SNAPSHOT"
+gitVersioning.apply {
+    refs {
+        branch(".+") { version = "\${ref}-SNAPSHOT" }
+        tag("v(?<version>.*)") { version = "\${ref.version}" }
+    }
+
+    // optional fallback configuration in case of no matching ref configuration
+    rev { version = "\${commit}" }
+}
 
 tasks.wrapper {
     gradleVersion = "7.3.3"
